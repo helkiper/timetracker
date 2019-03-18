80da55f8db355ed55693ff390b5cca7136749141
Hellkiper <helkiper@gmail.com>
2019-03-18 13:49:38 +0200

    made content refreshing by AJAX on a-click and form-submit

--- src/AppBundle/Resources/public/js/main.js
+++ src/AppBundle/Resources/public/js/main.js
@@ -1,3 +1,27 @@
 $(function () {
-    //TODO: implement
+    $('#content')
+        .on('click', 'a', AJAXEventHandler)
+        .on('submit', 'form', AJAXEventHandler);
+
+    function AJAXEventHandler(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        var target = $(event.target);
+        var options = {dataType: 'html'};
+
+        if (target.is('a')) {
+            options.url = target.attr('href');
+        } else if (target.is('form')) {
+            options.url = target.attr('action');
+            options.type = target.attr('method');
+            options.data = target.serialize();
+        }
+
+        $.ajax(options).done(function (data) {
+            $('#content').html(data);
+        }).catch(function (reason) {
+            alert(reason);
+        });
+    }
 });
